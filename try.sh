18705370f3b285382b4c9f9dca924dd05ee1d6f4
Gurjots <guri.singh346@gmail.com>
2014-07-02 22:55:00 +0530

    Working

--- try.sh
+++ try.sh
@@ -1,19 +1,25 @@
+
 #!/bin/bash
+
+
+echo Content-type: text/html
+echo
 
 # removing old files.
 rm -f ff.g
 rm -f rt*
+rm -f *.pix
+rm -f temp*
 
 rt=rtFile
 
-cat <<EOF | mged -c ff.g
+cat <<EOF | env /usr/brlcad/dev-7.25.0/bin//mged -c try.g
 in s rcc 4 0 0 0 1 0 2
 in s1 rpp 1 2 0 5 0 1
 in s2 rpp 6 7 0 5 0 1
 in s3 rcc 1 4 .5 1 0 0 .4
 in s4 rcc 6 4 .5 1 0 0 .4 
-in s5 rcc 4 0 0 0 1 0 2  
-
+in s5 rcc 4 0 0 0 1 0 1 
 r reg u s u s1 u s2 
 r reg1 u reg - s3 - s4 - s5 
 mater reg1 plastic 255 0 0 0
@@ -22,8 +28,14 @@ ae 115 310
 saveview $rt
 EOF
 
+sed '2cenv /usr/brlcad/dev-7.25.0//bin/rt -M \\' $rt > tempFile1
+
+rm $rt
+
+mv tempFile1 $rt
+
 # give executable permissions to raytrace file.
-chmod 755 $rt
+chmod 777 $rt
 
 # executing raytrace file. This will produce raw image in .pix format
 # and a log
@@ -31,9 +43,13 @@ chmod 755 $rt
 sh $rt
 
 # converting .pix file to png image using BRLCAD commands.
-pix-png < $rt.pix > $rt.png
+env /usr/brlcad/dev-7.25.0//bin/pix-png < $rt.pix > $rt.png
 
 # open png image in a frame buffer. Currently not required.
 #env /usr/brlcad/bin/png-fb $rt.png
 
-shotwell $rt.png
+chmod 777 $rt.png
+
+cp $rt.png ../
+
+echo "<img src=../$rt.png>"
